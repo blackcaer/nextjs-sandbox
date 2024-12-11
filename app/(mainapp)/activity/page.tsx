5c9ab582e261bdc4dccba7e5291aff72809fa847
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-11 19:16:22 +0100

    Added image

--- app/(mainapp)/activity/page.tsx
+++ app/(mainapp)/activity/page.tsx
@@ -3,7 +3,10 @@ import UserActivity from "@/app/ui/activity/user_activity";
 
 export default function Page() {
   return (
-    <div className="flex flex-col md:flex-row md:overflow-hidden">
+    <div className="flex flex-col md:flex-col gap-4 md:overflow-hidden">
+      <UserActivity />
+      <UserActivity />
+      <UserActivity />
       <UserActivity />
     </div>
   );
--- app/ui/activity/user_activity.tsx
+++ app/ui/activity/user_activity.tsx
@@ -7,38 +7,57 @@ export default function UserActivity() {
     id: 1,
     name: "John Nowak",
     amount: 1,
-    email: 1,
-    image_url: "/avatars/avatar1.png",
+    email: "example@email.com",
+    avatar_img_url: "/avatars/avatar1.png",
+    route_img_url: "/example/route1.png",
   };
 
   const i = 0;
   return (
-    <div
-      key={userData.id}
-      className={clsx("flex flex-row items-center justify-between p-8 border w-full")}
-    >
-      <div className="flex items-center">
-        <Image
-          src={userData.image_url}
-          alt={`${userData.name}'s profile picture`}
-          className="mr-4 rounded-full"
-          width={32}
-          height={32}
-        />
-        <div className="min-w-0">
-          <p className="truncate text-sm font-semibold md:text-base">
-            {userData.name}
-          </p>
-          <p className="hidden text-sm text-gray-500 sm:block">
-            {userData.email}
-          </p>
+    <div key={userData.id} className="flex flex-col border w-full">
+      <div
+        className="flex flex-row items-center justify-between px-7 pt-7 pb-4 w-full"
+      >
+        <div className="flex items-center">
+          <Image
+            src={userData.avatar_img_url}
+            alt={`${userData.name}'s profile picture`}
+            className="mr-4 rounded-full"
+            width={40}
+            height={40}
+          />
+          <div className="min-w-0 mr-2">
+            <p className="truncate text-sm font-semibold md:text-base">
+              {userData.name}
+            </p>
+            <p className="hidden text-sm text-gray-500 sm:block">
+              {userData.email}
+            </p>
+          </div>
+
+
+        </div>
+        <div className={`truncate text-sm font-medium md:text-base`}>
+          <p>{21321}</p>
         </div>
       </div>
-      <p
-            className={`truncate text-sm font-medium md:text-base`}
-        >
-            {21321}
+
+      <hr></hr>
+      <div className="flex flex-col w-full px-7 py-4">
+        
+        <Image
+        src={userData.route_img_url}
+        alt={`${userData.name} route image`}
+        width={400}
+        height={400}
+        className="block rounded-xl my-2"
+        />
+        
+        <p>
+        Lorem ipsum dolor sit amet
         </p>
+
+      </div>
     </div>
   );
 }
